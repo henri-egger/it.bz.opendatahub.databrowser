a205d5b2781ba5ec2cc6ecbc21f18c7a1e10e7ea
henri <henri@moosbauer.com>
2022-11-13 11:14:30 +0100

    add map test

--- databrowser/src/domain/cellComponents/components/cells/mapCell/mapCell.vue
+++ databrowser/src/domain/cellComponents/components/cells/mapCell/mapCell.vue
@@ -3,37 +3,40 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps, onMounted, withDefaults } from 'vue';
+import { defineProps, onMounted, withDefaults, toRefs } from 'vue';
 import 'leaflet/dist/leaflet.css';
 import leaflet from 'leaflet';
 
 const props = withDefaults(
   defineProps<{
     pos?: {
-      Gpstype: string,
-      Altitude: number,
-      Latitude: number,
-      Longitude: number,
-      AltitudeUnitofMeasure: string,
-    },
+      Gpstype: string;
+      Altitude: number;
+      Latitude: number;
+      Longitude: number;
+      AltitudeUnitofMeasure: string;
+    };
   }>(),
   {
     pos: undefined,
   }
 );
 
-let lat = props.pos.Latitude;
-let lon = props.pos.Longitude;
+const { Latitude, Longitude } = toRefs(props.pos);
+
+// let lat = props.pos.Latitude;
+// let lon = props.pos.Longitude;
 let mymap: leaflet.Map;
 
 onMounted(() => {
-  mymap = leaflet.map('mymap').setView([lat, lon], 13)
-  leaflet.marker([lat, lon]).addTo(mymap);
+  mymap = leaflet.map('mymap').setView([Latitude.value, Longitude.value], 13);
+  leaflet.marker([Latitude.value, Longitude.value]).addTo(mymap);
 
-  leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '© OpenStreetMap contributors'
-  }).addTo(mymap);
+  leaflet
+    .tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap contributors',
+    })
+    .addTo(mymap);
 });
-
 </script>
